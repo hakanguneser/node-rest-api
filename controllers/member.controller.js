a72f1b9b6b82a5f337230279f57a84a7ff2fdfd2
hakanguneser <hakan.guneser@gmail.com>
2021-01-30 00:00:00 +0300

    controller created for GET and implemented to route

---
+++ controllers/member.controller.js
@@ -0,0 +1,4 @@
+
+exports.getAllMembers = (req,res,next)=>{
+    res.json('Member route get all function called');
+};
--- routes/member.routes.js
+++ routes/member.routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const memberController = require('../controllers/member.controller');
 
-router.get('/', async (req, res, next) => {
-    res.json('Member route get all function called');
-});
+
+router.get('/', memberController.getAllMembers);
 
 module.exports = router;
