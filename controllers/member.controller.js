fc2bbc9ce02a1d35d3bbfaeaf5dad34a02fb6e2d
hakanguneser <hakan.guneser@gmail.com>
2021-01-30 00:56:00 +0300

    /member POST method development

--- controllers/member.controller.js
+++ controllers/member.controller.js
@@ -1,9 +1,15 @@
+const MemberModel = require('../model/member.model');
 
-exports.getAllMembers = (req,res,next)=>{
+exports.getAllMembers = (req, res, next) => {
     res.json('Member route get all function called');
 };
 
 
-exports.createMember = (req,res,next)=>{
-    res.json('Member route get all function called');
+exports.createMember = async (req, res, next) => {
+    try {
+        const createdMember = await MemberModel.create(req.body);
+        res.status(201).json(createdMember);
+    } catch (error) {
+        next(error);
+    }
 };
