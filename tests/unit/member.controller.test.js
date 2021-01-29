4b791443035fbbf8d7ddb6d4345f6aa56d9030d0
hakanguneser <hakan.guneser@gmail.com>
2021-01-30 00:55:04 +0300

    Unit test imporevement with mock data

--- tests/unit/member.controller.test.js
+++ tests/unit/member.controller.test.js
@@ -1,14 +1,34 @@
-const MemberController =require('../../controllers/member.controller');
+const httpMocks = require('node-mocks-http');
+const MemberController = require('../../controllers/member.controller');
+const MemberModel = require('../../model/member.model');
 
-describe('MemberContoller.getAllMembers',()=>{
-    it('should have a getAllMembers function',()=>{
+const newMember = require('../mock-data/new-member.json');
+
+jest.mock('../../model/member.model');
+
+let req, res, next;
+
+beforeEach(() => {
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    next = jest.fn();
+});
+
+
+describe('MemberContoller.getAllMembers', () => {
+    it('should have a getAllMembers function', () => {
         expect(typeof MemberController.getAllMembers).toBe('function');
     });
 });
 
 
-describe('MemberContoller.createMember',()=>{
-    it('should have a createMember function',()=>{
+describe('MemberContoller.createMember', () => {
+    it('should have a createMember function', () => {
         expect(typeof MemberController.createMember).toBe('function');
     });
+    it('should call MemberModel.create', () => {
+        req.body = newMember;
+        MemberController.createMember(req, res, next);
+        expect(MemberModel.create).toBeCalledWith(newMember);
+    })
 });
