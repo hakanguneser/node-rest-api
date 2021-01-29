6459e58e7ac555a2ef1431896c9fa03cfe04a9ee
hakanguneser <hakan.guneser@gmail.com>
2021-01-30 00:19:34 +0300

    DB connection properties developed

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const memberRoutes = require('./routes/member.routes');
+const mongoose = require('mongoose');
 const app = express();
-const PORT = 3000;
 
-app.use('/members',memberRoutes);
+const PORT = 3000;
+const MONGO_DB_URI = 'mongodb://localhost:27017/node-db';
+
+app.use('/members', memberRoutes);
 
 app.use('/', (req, res, next) => {
     console.log("123");
     res.json("Welcome to base URL");
+});
+
+mongoose.connect(MONGO_DB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}, () => {
+    console.log('DB connection done');
 });
 
 app.listen(PORT, () => {
